539f7e54e67f2e61d1902943798302dae13ef34f
Raginiambi <155716940+Raginiambi@users.noreply.github.com>
2024-07-09 11:39:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let button = document.querySelector("button");
 let msg = document.querySelectorAll("input");
 let contact = document.querySelector("#contact-div");
 let end = document.querySelector("#end"); 
+let contact_sec = document.querySelector("#contact_main");
 
 button.addEventListener("click",()=>{
    contact.style.visibility = "hidden";
    end.style.visibility = "visible";
    end.style.marginLeft ="0px";
    end.style.paddingTop = "250px";
+   contact_sec.style.height = "640px";
  
  
 })
