0bd34f799ac1bc38d25e1ad16a9fbc56eb879fc1
Raginiambi <155716940+Raginiambi@users.noreply.github.com>
2024-07-09 22:30:05 +0530

    Update index.js
    set the contact display to none

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let button = document.querySelector("button");
+let button = document.querySelector("#send");
 let msg = document.querySelectorAll("input");
 let contact = document.querySelector("#contact-div");
 let end = document.querySelector("#end"); 
 let contact_sec = document.querySelector("#contact_main");
 
 button.addEventListener("click",()=>{
-   contact.style.visibility = "hidden";
+   contact.style.display = "none";
    end.style.visibility = "visible";
    end.style.marginLeft ="0px";
    end.style.paddingTop = "250px";
    contact_sec.style.height = "640px";
+   
  
  
 })
